00625b5ea23ebbccff525dff57ebd5b783a99544
Camila Brito <camilam.brito@outlook.com.br>
2026-05-26 22:27:44 -0300

    Excluindo video.

--- novo_video.php
+++ novo_video.php
@@ -4,12 +4,12 @@ $_caminho = __DIR__.'/banco.sqlite';
 $pdo =  new PDO("sqlite:$_caminho");
 
 $_url = filter_input(INPUT_POST,'url',FILTER_VALIDATE_URL);
-if($_url === false) {
+$_titulo = filter_input(INPUT_POST,'titulo');
+
+if($_url === false || $_titulo === false) {
     header('Location: /index.php?sucesso=0');
     exit();
 }
-$_titulo = filter_input($_POST['titulo']);
-
 
 $sql = "INSERT INTO videos (url, title) VALUES(?, ?)";
 $statement = $pdo->prepare($sql);
